be887ed7be5930c4f74c9fd34ab81f8b7a3602c7
kenny <blestkenny@yandex.ru>
2023-03-31 18:37:52 +0300

    12 lesson

--- src/App.js
+++ src/App.js
@@ -7,24 +7,49 @@ import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import {Route, Routes} from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./redux/slices/filterSlice";
 
 export const SearchContext = createContext()
 
 function App() {
     const [searchValue, setSearchValue] = React.useState('')
+    const filter = useSelector((state)=> state.filter.value)
+    const dispatch = useDispatch()
 
     return (
         <div className="wrapper">
-            <SearchContext.Provider value={{ searchValue, setSearchValue }}>
-                <Header/>
-                <div className="content">
-                    <Routes>
-                        <Route path="" element={<Home/>}/>
-                        <Route path="/cart" element={<Cart/>}/>
-                        <Route path="*" element={<NotFound/>}/>
-                    </Routes>
-                </div>
-            </SearchContext.Provider>
+            <div>
+                <button
+                    aria-label="Increment value"
+                    onClick={() => dispatch(increment())}
+                >
+                    Increment
+                </button>
+                <span>{filter}</span>
+                <button
+                    aria-label="Decrement value"
+                    onClick={() => dispatch(decrement())}
+                >
+                    Decrement
+                </button>
+            </div>
+
+
+
+
+
+
+            {/*<SearchContext.Provider value={{ searchValue, setSearchValue }}>*/}
+            {/*    <Header/>*/}
+            {/*    <div className="content">*/}
+            {/*        <Routes>*/}
+            {/*            <Route path="" element={<Home/>}/>*/}
+            {/*            <Route path="/cart" element={<Cart/>}/>*/}
+            {/*            <Route path="*" element={<NotFound/>}/>*/}
+            {/*        </Routes>*/}
+            {/*    </div>*/}
+            {/*</SearchContext.Provider>*/}
         </div>
     );
 }
